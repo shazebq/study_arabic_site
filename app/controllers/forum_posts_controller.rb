7293494c254e12ec333524273f5d0cc43505a88b
Shazeb Qadir <shazebq@gmail.com>
2012-09-19 22:01:40 -0700

    add forum posts controller

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,7 +1,7 @@
 class ForumPostsController < ApplicationController
 
   def create
-    return render text: "hell"
+    return render text: "hello"
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,5 @@ class Category < ActiveRecord::Base
   has_many :forum_posts
   belongs_to :parent_category
 
-  attr_accessible :name, :parent
-
+  attr_accessible :name, :parent_category_id
 end
