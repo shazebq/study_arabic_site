5adfe9bfbae02ae3ccc96baa92dbfd0130afe30e
Shazeb Qadir <shazebq@gmail.com>
2012-11-24 14:39:54 -0800

    counter_cache: true for Vote and View model

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,7 +1,7 @@
 class View < ActiveRecord::Base
   attr_accessible :viewable_id, :viewable_type, :ip_address, :session_id
 
-  belongs_to :viewable, polymorphic: true
+  belongs_to :viewable, polymorphic: true, counter_cache: true
 
   validates_uniqueness_of :ip_address, :scope => [:viewable_id, :viewable_type, :session_id]
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
   attr_accessible :user_id, :voteable_id, :voteable_type
 
-  belongs_to :voteable, polymorphic: true
+  belongs_to :voteable, polymorphic: true, counter_cache: true
 
   validates_uniqueness_of :user_id, :scope => [:voteable_id, :voteable_type]
 end
