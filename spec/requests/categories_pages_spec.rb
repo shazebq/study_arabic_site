843630f1510262511de615363fbec68b711222f6
Shazeb Qadir <shazebq@gmail.com>
2012-12-30 19:48:50 -0800

    add links to categories on category index page

--- spec/requests/categories_pages_spec.rb
+++ spec/requests/categories_pages_spec.rb
@@ -24,6 +24,24 @@ describe "Category Pages" do
       it { should have_selector(".categories_div", text: "Books")}
       it { should have_selector(".categories_div", text: "Arabic Centers")}
     end
+
+    describe "category links" do
+      it { should have_selector("a", text: "Arabic Language") }
+      it { should have_selector("a", text: "Arabic Centers") }
+
+      describe "clicking on a category link" do
+        it "should redirect to forum post page of the category" do
+          click_link("Arabic Centers")
+          current_path.should == category_forum_posts_path(child1)
+        end
+
+        it "should redirect to forum post page of a parent category" do
+          click_link("Arabic Language")
+          current_path.should == category_forum_posts_path(parent)
+        end
+      end
+    end
+
   end
 
 
