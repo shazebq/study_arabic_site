0713077f734816d58ccaa6937e029e2f2a8ea3a4
Shazeb Qadir <shazebq@gmail.com>
2013-02-05 20:09:45 -0800

    add new test for generate_stars helper to deal with half stars

--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -13,6 +13,13 @@ describe ReviewsHelper do
     it "returns an html snippet with the zero filled in star our of five" do
       generate_stars(0).should == "<i class='icon-star-empty'></i><i class='icon-star-empty'></i><i class='icon-star-empty'></i><i class='icon-star-empty'></i><i class='icon-star-empty'></i>"
     end
+
+    context "when there is a half number as the argument" do
+      it "returns an html snippet with a half a star" do
+        generate_stars(1.5).should == "<i class='icon-star'></i><i class='icon-star-half'></i>"
+      end
+    end
+
   end
 end
 
