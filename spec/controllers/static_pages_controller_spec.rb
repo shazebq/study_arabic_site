ecb330f4b7087c8eefd8374ab1cd43b3139f64d4
Shazeb Qadir <shazebq@gmail.com>
2013-02-12 10:50:15 -0800

    fix typo and failing tests

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,18 +2,5 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'about_us'" do
-    it "returns http success" do
-      get 'about_us'
-      response.should be_success
-    end
-  end
 
 end
