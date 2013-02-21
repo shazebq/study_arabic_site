19e56e36ad3d55b726ad0eae48dbc3f8b7850282
Shazeb Qadir <shazebq@gmail.com>
2013-02-20 18:59:07 -0800

    add nested form for creating new center record

--- app/controllers/centers_controller.rb
+++ app/controllers/centers_controller.rb
@@ -1,5 +1,11 @@
 class CentersController < ApplicationController
   def new
     @center = Center.new
+    @address = @center.build_address
+  end
+
+  def create
+    @center = Center.create(params[:center])
+    return render text: "good job"
   end
 end
--- spec/features/centers_new_page_spec.rb
+++ spec/features/centers_new_page_spec.rb
@@ -18,6 +18,24 @@ describe "new center page" do
       page.should have_selector("#center_description")
     end
   end
+
+  describe "submitting a new center/program" do
+    before :each do
+      fill_in "center_name", with: "Diwan Arabic Center"
+      fill_in "center_description", with: "A wonderful arabic center where you can learn a whole lot!"
+      fill_in "center_address_attributes_address_line", with: "123 Najeeb Mahfouz Street"
+      fill_in "center_address_attributes_city_id", with: 2 
+      fill_in "center_address_attributes_country_id", with: 5
+    end
+
+    specify "clicking the submit button should create a new center record" do
+      expect { click_button "Submit" }.to change(Center, :count)
+    end
+    
+    specify "clicking the submit button should create a new address record as well" do
+      expect { click_button "Submit" }.to change(Address, :count)
+    end
+  end
 end
 
 # comments
