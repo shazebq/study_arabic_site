10915481b91f06245d7c991af0a2f8d9ba882966
Shazeb Qadir <shazebq@gmail.com>
2013-02-21 20:10:45 -0800

    add tries to account for centers without pictures

--- spec/factories/centers.rb
+++ spec/factories/centers.rb
@@ -2,19 +2,19 @@
 
 FactoryGirl.define do
   factory :center do
-    name "MyText"
-    description "MyText"
+    name "Super Cool Arabic Center"
+    description "An excellent top notch arabic center"
     short_term false
     long_term false
-    website "MyString"
-    email "MyString"
-    phone_number "MyString"
+    website "www.coolarabiccenter.com"
+    email "bob@coolarabic.com"
+    phone_number "405-234-2939"
     address_id 1
     program_length "MyText"
     user_id 1
     private_instruction false
     group_instruction false
-    year_established 1
+    year_established 1998
     price_per_hour_private "9.99"
     price_per_hour_group "9.99"
     total_price "9.99"
--- spec/features/centers_show_page_spec.rb
+++ spec/features/centers_show_page_spec.rb
@@ -1,6 +1,9 @@
 require "spec_helper"
 
 describe "centers show page" do
+  let!(:country) { FactoryGirl.create(:country) }
+  let!(:city) { FactoryGirl.create(:city, country_id: country.id) }
+  let!(:address) { FactoryGirl.create(:address, country_id: country.id, city_id: city.id) }
   let!(:center) { FactoryGirl.create(:center, name: "Number 1 Arabic Center") }
 
   before :each do
@@ -12,4 +15,11 @@ describe "centers show page" do
       page.html.should have_selector("title", text: "Number 1 Arabic Center")
     end
   end
+
+  describe "general contents" do
+    it "should include the name of the center" do
+      page.should have_content(center.name)
+    end
+  end
+
 end
