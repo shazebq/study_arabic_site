6858a3ef5f6548af15044ad770a2446c4fe58b50
Shazeb Qadir <shazebq@gmail.com>
2013-02-24 12:57:43 -0800

    successful test of using ajax to load profile list

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,3 +1,5 @@
+// this is for the twitter bootstrap image carousel
+// by default it revolves the images, this is so that it doesn't
 $(document).ready(function() {
     $('.carousel').carousel( { pause: true, interval: false }); 	
 })
--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,2 +1,9 @@
 class TeacherProfilesController < ProfilesController
+  def index
+    if params[:ratings_option]
+      @teacher_profiles = TeacherProfile.order_by_reviews
+    else
+      @teacher_profiles = TeacherProfile.all
+    end
+  end
 end
