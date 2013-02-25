0bd7bc4232346ebf8f715df6dfc1e3b5292be5f3
Shazeb Qadir <shazebq@gmail.com>
2013-02-24 16:10:03 -0800

    change id of rating sorting options to match method names in rails

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,7 +1,7 @@
 class TeacherProfilesController < ProfilesController
   def index
     if params[:ratings_option]
-      @teacher_profiles = TeacherProfile.order_by_reviews
+      @teacher_profiles = TeacherProfile.send(params[:ratings_option])
     else
       @teacher_profiles = TeacherProfile.all
     end
