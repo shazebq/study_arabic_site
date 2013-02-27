cb1a39453b2a3691d72e90d762e0a561f1caad0c
Shazeb Qadir <shazebq@gmail.com>
2013-02-27 12:00:23 -0800

    get javascript ajax calls working for all filter criteria

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -4,6 +4,8 @@ class TeacherProfilesController < ProfilesController
     if params[:ratings_option] && params[:instruction_option]
       condition = generator.bool_generator(params[:instruction_option])
       @teacher_profiles = TeacherProfile.where(condition[0], condition[1])
+    elsif params[:price_option]
+      @teacher_profiles = TeacherProfile.by_price(params[:price_option])
     else
       @teacher_profiles = TeacherProfile.order_by_average_rating + TeacherProfile.zero_review_records
     end
