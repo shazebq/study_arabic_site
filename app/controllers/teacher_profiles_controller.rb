9dec7aa649a3d9b61c733d4a0e22eed685a05102
Shazeb Qadir <shazebq@gmail.com>
2013-02-27 18:53:03 -0800

    updates

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -1,25 +1,21 @@
 class TeacherProfilesController < ProfilesController
   def index
-    t = TeacherProfile.arel_table
-    #@teacher_profiles = TeacherProfile.order_by_average_rating.where(t[:online].eq("true").or(t[:in_person].eq("true")))
-    if params[:instruction_option] || params[:ratings_option]
+    if params[:ratings_option] == "order_by_reviews"
       @teacher_profiles = TeacherProfile.send(params[:ratings_option]).instruction_type(params[:instruction_option]).
                           by_price(params[:price_option])
+
+    elsif params[:ratings_option] == "order_by_average_rating"
+      @teacher_profiles = TeacherProfile.send(params[:ratings_option]).instruction_type(params[:instruction_option]).
+                          by_price(params[:price_option]) + TeacherProfile.zero_review_records
     else
-      @teacher_profiles = TeacherProfile.order_by_average_rating
+      @teacher_profiles = TeacherProfile.order_by_average_rating + TeacherProfile.zero_review_records
     end
-    #generator = ConditionGenerator.new
-    #if params[:ratings_option] || params[:instruction_option]
-    #  #condition = generator.bool_generator(params[:instruction_option])
-    #  #@teacher_profiles = TeacherProfile.where(condition[0], condition[1])
-    #  return render text: params
-    #elsif params[:price_option]
-    #  @teacher_profiles = TeacherProfile.by_price(params[:price_option])
-    #else
-    #  @teacher_profiles = TeacherProfile.order_by_average_rating + TeacherProfile.zero_review_records
-    #end
   end
 end
 
 
 # {"instruction_option"=>["in_person", "online"], "ratings_option"=>"order_by_average_rating", "price_option"=>"20", "action"=>"index", "controller"=>"teacher_profiles"}
+#  def chain_methods 
+#    ["instruction_type", "by_price"].inject() { |initial, additional| initial.send(additional, params[additional.to_sym])
+#  end
+
