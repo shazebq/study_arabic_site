c4471653421288bf04d7699a982a261ae5b82669
Shazeb Qadir <shazebq@gmail.com>
2013-03-06 22:29:29 -0800

    add authorization to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_filter(:only => [:destroy, :update]) { |c| c.require_user_is_owner(params[:controller], params[:id]) }
 
   def new
     @reviewable = get_reviewable(params)
