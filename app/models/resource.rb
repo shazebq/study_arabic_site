1c07f74f48d00d88494defce76d8c2302cf73ea7
Shazeb Qadir <shazebq@gmail.com>
2013-03-12 21:54:33 -0700

    fix resource validation and styles

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -34,9 +34,8 @@ class Resource < ActiveRecord::Base
   validates_attachment_content_type(:resource_file, content_type: ["image/jpeg", "image/jpg", "application/pdf", "application/msword", "text/plain"])
 
   validates :title, :description, :category_ids, presence: true
-
-  validates :title, length: { maximum: 65 }
-  validates :description, length: { maximum: 5000 }
+  validates :title, length: { maximum: 130 }
+  validates :description, length: { maximum: 2000 }
 
   SCOPES = ["most_recent", "most_views", "most_votes", "most_downloads"]
 
