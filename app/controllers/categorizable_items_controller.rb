f8ff317427bd9f4721c23cabfc141e49a03fa25b
Shazeb Qadir <shazebq@gmail.com>
2013-03-13 17:43:03 -0700

    add pagination to rest of index pages

--- app/controllers/categorizable_items_controller.rb
+++ app/controllers/categorizable_items_controller.rb
@@ -5,19 +5,19 @@ class CategorizableItemsController < ApplicationController
     if params[:category_id]
       @category = CategoryParent.find(params[:category_id])
         if @category.categories.any?  # if it's a parent category
-          @items = @category.collect_all_posts(params[:controller]).send(@current_scope) 
+          @items = @category.collect_all_posts(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
         else # if it's not a parent category
-          @items = @category.send(params[:controller]).send(@current_scope)
+          @items = @category.send(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
         end
     else
       # call appropriate scope here
-      @items = params[:controller].classify.constantize.send(@current_scope)
+      @items = params[:controller].classify.constantize.send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
     end
   end 
 
   def search
     @scopes = params[:controller].classify.constantize::SCOPES
-    @items = params[:controller].classify.constantize.text_search(params[:query]) 
+    @items = params[:controller].classify.constantize.text_search(params[:query]).paginate(page: params[:page], per_page: PER_PAGE)  
     render "index"
   end
 end
