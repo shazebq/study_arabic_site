270c49986e256127b1ff1a175670c2dbc0bba849
Shazeb Qadir <shazebq@gmail.com>
2013-03-27 14:58:11 -0700

    add chosen to country slector

--- app/assets/javascripts/chosen/chosen.js
+++ app/assets/javascripts/chosen/chosen.js
@@ -1,5 +1,5 @@
 jQuery(function($){
     $('.chzn-select').chosen();
     $('#teacher_profile_language_ids').chosen();
-    $('#teacher_profile_user_attributes_country_id').chosen();
+    $('.country_select').chosen();
 });
