e9503783e81b605e932ea958e8f068699dae30c9
Shazeb Qadir <shazebq@gmail.com>
2013-03-27 22:49:05 -0700

    add ajax loader

--- app/assets/javascripts/misc.js
+++ app/assets/javascripts/misc.js
@@ -1,1 +1,13 @@
+$(document).ready(function() { 
+  $('#ajax_spinner_div').hide();
+  jQuery.ajaxSetup({
+    beforeSend: function() {
+     $('#ajax_spinner_div').show();
+    },
+    complete: function(){
+     $('#ajax_spinner_div').hide();
+    },
+    success: function() {}
+  });
 
+});
