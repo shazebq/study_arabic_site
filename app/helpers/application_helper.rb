259a92227a744a5a4fe87a02a56de31002131101
Shazeb Qadir <shazebq@gmail.com>
2013-03-29 22:45:30 -0700

    update article show view with new widget

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -83,14 +83,14 @@ module ApplicationHelper
   def modified_sf(text, html_options={}, options={})
     text = '' if text.nil?
     text = text.dup
-    start_tag = tag('div', html_options, true)
+    start_tag = tag('span', html_options, true)
     text = sanitize(text) unless options[:sanitize] == false
     text = text.to_str
     text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
     text.gsub!(/\n\n+/, "</p>\n\n#{start_tag}")  # 2+ newline  -> paragraph
     text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
     text.insert 0, start_tag
-    text.html_safe.safe_concat("</div>")
+    text.html_safe.safe_concat("</span>")
   end
   
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ActiveRecord::Base
 
   belongs_to :commentable, polymorphic: :true, counter_cache: true
   belongs_to :user
-  
+
+  # to deal with the Comment.new dilemma where the new blank comment record in the controller is counted undesirably
   scope :saved_records, where("ID IS NOT ?", nil)
 
   validates :user_id, :content, presence: true
