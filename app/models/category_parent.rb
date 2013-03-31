b9e09db4e99521140499aac33c4f65ece961f3fb
Shazeb Qadir <shazebq@gmail.com>
2013-03-31 12:40:00 -0700

    users can select parent general categories

--- app/models/category_parent.rb
+++ app/models/category_parent.rb
@@ -4,6 +4,8 @@ class CategoryParent < Category
   # get posts of all children categories
   def collect_all_posts(item)
     category_ids = self.categories.map { |c| c.id }
+    # in order to get the parent categories general items as well
+    category_ids << self.id
     #need a way to get distinct posts or resources
     item.classify.constantize.joins(:categories).where("category_id IN (?)", category_ids).uniq
   end
