83760e180d6a24d2a10e5135f28939d673734e8a
Shazeb Qadir <shazebq@gmail.com>
2013-03-31 12:06:03 -0700

    add icon for situation when boolean value is nil

--- app/helpers/user_pages_helper.rb
+++ app/helpers/user_pages_helper.rb
@@ -2,8 +2,10 @@ module UserPagesHelper
   def bool_display(value)
     if value == true
       "<i class='icon-check icon-large'></i>".html_safe
-    else
+    elsif value == false
       "<i class='icon-check-empty icon-large'></i>".html_safe
+    else
+      "<i class='icon-question-sign icon-large'></i>".html_safe
     end
   end
 end
