82e7a581ff977d70423b07b41ecedda14a36fc01
Shazeb Qadir <shazebq@gmail.com>
2013-03-30 20:53:47 -0700

    add try method for before voted_up?

--- app/helpers/voting_helper.rb
+++ app/helpers/voting_helper.rb
@@ -1,7 +1,7 @@
 module VotingHelper
   # for making an arrow appear selected if the user has already voted for the item
   def arrow_class(item)
-    if current_user.voted_up?(item)
+    if current_user.try(:voted_up?, item)
       # due to an answers being on the same page as forum posts
       return "selected_arrow_answer" if item.kind_of? Answer 
       return "selected_arrow"
