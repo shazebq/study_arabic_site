8c5114ab17b43a5d401811343a9504e7e00a71b3
Shazeb Qadir <shazebq@gmail.com>
2013-04-01 21:24:43 -0700

    add contact information on teacher profile page conditionally

--- app/models/teacher_profile.rb
+++ app/models/teacher_profile.rb
@@ -6,7 +6,9 @@ class TeacherProfile < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search, against: [:specialties, :field_of_study, :university],
     using: {tsearch: {dictionary: "english"}},
-    associated_against: {reviews: :content, user: [:first_name, :last_name, :bio]} # needed so it searches associated records as well
+    associated_against: {reviews: :content, user: [:first_name, :last_name, :bio],
+                         languages: [:name], degrees: [:name], cities: [:name]} # needed so it searches associated records as well
+
   #multisearchable :against => [:specialties, :field_of_study, :university]
 
   after_initialize :init
