61c24a0d19ff92ccf756fd7cf7e188e9862d90a7
Shazeb Qadir <shazebq@gmail.com>
2013-04-21 15:44:43 -0700

    quick fix for eager loading associated teacher_profile tables

--- app/controllers/teacher_profiles_controller.rb
+++ app/controllers/teacher_profiles_controller.rb
@@ -2,10 +2,10 @@ class TeacherProfilesController < ProfilesController
   def index
     @countries = Country.joins(:users).where("users.profile_type = ?", "TeacherProfile").uniq # all the countries that teachers are from (unique)
     if params[:ratings_option] == "order_by_reviews"
-      @teacher_profiles = TeacherProfile.send(params[:ratings_option]).chain_scopes(params).include_associated.paginate(page: params[:page], per_page: PER_PAGE)
+      @teacher_profiles = TeacherProfile.send(params[:ratings_option]).include_associated.chain_scopes(params).paginate(page: params[:page], per_page: PER_PAGE)
     elsif params[:ratings_option] == "order_by_average_rating"
-      @teacher_profiles = (TeacherProfile.send(params[:ratings_option]).chain_scopes(params).include_associated + 
-                          TeacherProfile.zero_review_records.chain_scopes(params)).include_associated.paginate(page: params[:page], per_page: PER_PAGE)
+      @teacher_profiles = (TeacherProfile.send(params[:ratings_option]).include_associated.chain_scopes(params) + 
+                          TeacherProfile.zero_review_records.include_associated.chain_scopes(params)).paginate(page: params[:page], per_page: PER_PAGE)
     else
       @teacher_profiles = (TeacherProfile.order_by_average_rating.include_associated + 
                           TeacherProfile.zero_review_records.include_associated).paginate(page: params[:page], per_page: PER_PAGE)
