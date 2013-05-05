48f7ac24c724b8a648ff545a21a63e9cbed7f9d4
Shazeb Qadir <shazebq@gmail.com>
2013-05-04 20:58:25 -0700

    add user activity partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @forum_posts = @user.forum_posts
+    @answers = @user.answers
+    @reviews = @user.reviews
+    @comments = @user.comments
   end
 end
