5543b8ff36d5b025f44a43e587c7fa487b84d68b
Shazeb Qadir <sqadir@tribune.com>
2013-07-24 16:24:24 -0700

    start coding notification sentence

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,5 @@
 module NotificationsHelper
-  VERBS = [:answer => "answered", :vote  => "up voted", :comment => "commented on", :review => "reviewed"]
+  VERBS = {:answer => "answered", :vote  => "up voted", :comment => "commented on", :review => "reviewed"}
   STANDARD_ITEMS = ["Answer", "Review", "Vote"]
 
   def notification_link(notification)
@@ -8,6 +8,9 @@ module NotificationsHelper
 
   def notification_sentence(notification)
     # jsmith answered your question 
+
+    verb = VERBS[notification.responsible_party_object_type.underscore.to_sym]
+    sentence = "#{notification.responsible_party.username} #{verb}"
   end
 
   def link(notification)
