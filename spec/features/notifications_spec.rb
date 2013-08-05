687e7d82b62fa1c8392a91d52bd007412d711974
Shazeb Qadir <sqadir@tribune.com>
2013-08-05 09:08:41 -0700

    add test for checking if notification is marked as checked after being clicked

--- spec/features/notifications_spec.rb
+++ spec/features/notifications_spec.rb
@@ -2,29 +2,31 @@ require "spec_helper"
 include Devise::TestHelpers
 
 describe "notifications" do
-  # test if they get marked as checked when they are checked
-  create_categories
   initialize_records
-  # create a notification with valid attributes
-  let!(:notification) { FactoryGirl.create(:notification, recipient_id: user.id, responsible_party_id: user1.id) }
-
-                                           #:recipient_object_id: forum_post.id, :recipient_object_type: "ForumPost", 
-                                           #responsible_party_object_id: answer1.id, responsible_party_object_type: "Answer") }
-
-  describe "a user clicks on one of his notifications" do
-
-    # something weird going on here...figure this out
-
-    before :each do
-      sign_in_user(user)
-      visit forum_post_path(forum_post)
-      save_and_open_page
-    end
-
-    it "is just a test" do
-      click_link "#{user1.username} answered your question"
-    end
+  create_categories
+  let!(:notification) { FactoryGirl.create(:notification, recipient_id: user.id, responsible_party_id: user1.id,
+                                          recipient_object_id: forum_post.id, recipient_object_type: "ForumPost",
+                                          responsible_party_object_id: answer1.id, responsible_party_object_type: "Answer") }
+  before do
+    sign_in_user(user)
+    visit forum_posts_path
   end
 
+  describe "clicking on a notification link" do
+    it "should change the notification's checked attribute to true" do
+      click_link "#{user1.username} answered your question"
+      notification.reload
+      notification.checked.should == true
+    end
+
+    it "should redirect the user the appropriate page" do
+      click_link "#{user1.username} answered your question"
+      current_path.should == forum_post_path(forum_post)
+    end
+  end
 end
+
+                                           
+                                           
+
 
