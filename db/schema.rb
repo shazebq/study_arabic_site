1db357900cff92a9c98f187a1eb8db9af6d6432b
Shazeb Qadir <sqadir@tribune.com>
2013-08-14 09:41:11 -0700

    run all new migrations after merging branches

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20130806230619) do
-=======
 ActiveRecord::Schema.define(:version => 20130813212149) do
->>>>>>> blog_editor
 
   create_table "addresses", :force => true do |t|
     t.text     "address_line"
