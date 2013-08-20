c4a621e1cae354825748ae5f39b724d536d42765
Shazeb Qadir <sqadir@tribune.com>
2013-08-20 15:43:05 -0700

    eliminate multiple queries in sidebar for getting counts of articles in each category via eager loading

--- app/controllers/categorizable_items_controller.rb
+++ app/controllers/categorizable_items_controller.rb
@@ -4,14 +4,14 @@ class CategorizableItemsController < ApplicationController
     @current_scope = params[:order_by] || "most_recent"  # default the scope to most_recent
     if params[:category_id]
       @category = CategoryParent.find(params[:category_id])
-        if @category.categories.any?  # if it's a parent category
-          @items = @category.collect_all_posts(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
-        else # if it's not a parent category
-          @items = @category.send(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
-        end
+      if @category.categories.any?  # if it's a parent category
+        @items = @category.collect_all_posts(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
+      else # if it's not a parent category
+        @items = @category.send(params[:controller]).send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
+      end
     else
       # call appropriate scope here
-      @items = params[:controller].classify.constantize.send(@current_scope).paginate(page: params[:page], per_page: PER_PAGE) 
+      @items = params[:controller].classify.constantize.send(@current_scope).includes(:categories).paginate(page: params[:page], per_page: PER_PAGE) 
     end
   end 
 
